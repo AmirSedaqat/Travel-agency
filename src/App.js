2be98bf4e41f9926a44c83ababfa35d762f7ca62
Amir Sedaqat <amirsedaghat.dev@gmail.com>
2022-07-03 05:31:51 +0430

    Add AppBar in App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import SearchAppBar from "./components/AppBar";
 const App = () => {
     return (
         <div>
-            <Container>
+                <SearchAppBar/>
+            <Container sx={{marginY:5}}>
                 <Grid container spacing={5}>
                     <TourCard />
                     <TourCard />
