e4be4911adc642e6e8872acfce8e83f699516e9b
Amir Sedaqat <amirsedaghat.dev@gmail.com>
2022-07-12 19:34:17 +0430

    Change color navbar again

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -12,8 +12,8 @@ import SearchIcon from "@mui/icons-material/Search";
 //react-router-dom
 import { Link } from "react-router-dom";
 //images & gif
-import logoL from '../assets/logoL.png'
-import travelGif from '../assets/Travel.gif'
+import logoL from "../assets/logoL.png";
+import travelGif from "../assets/Travel.gif";
 
 const Search = styled("div")(({ theme }) => ({
     position: "relative",
@@ -59,13 +59,15 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 const SearchAppBar = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{backgroundColor:'tomato'}}>
+            <AppBar position="static" sx={{ backgroundColor: "rgb(222, 84, 60)" }}>
                 <Toolbar>
                     <IconButton size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }}>
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
-                       <Link to='/'><img src={travelGif} alt="Logo" style={{height:'60px'}}/> <img src={logoL} alt="Logo Large"/> </Link>
+                        <Link to="/">
+                            <img src={travelGif} alt="Logo" style={{ height: "60px", position: "relative", top: "5px" }} /> <img src={logoL} alt="Logo Large" />{" "}
+                        </Link>
                     </Typography>
                     <Search>
                         <SearchIconWrapper>
